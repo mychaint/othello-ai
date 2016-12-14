ce7e3f68e082735242087ec4f180a4750d743876
Chen Gong <gong.chen@streetsine.com>
2016-12-15 00:10:16 +0800

    update

--- OthelloGUI.py
+++ OthelloGUI.py
@@ -1,15 +1,17 @@
 import Tkinter as tk
-import Othello
+from Othello import *
 
 class OthelloGUI:
     def __init__(self, master):
         self.master = master
         self.master.title('Othello')
+        self.gamehost = Othello(False)
+        
         self.centerWindow()
-    
+
     def centerWindow(self):
-        w = 800
-        h = 600
+        w = 700
+        h = 800
         sw = self.master.winfo_screenwidth()
         sh = self.master.winfo_screenheight()
         x = (sw - w)/2
