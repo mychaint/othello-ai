ae7aad69d9406c3ed073fc20b1c50d4f09e26e69
chengongData <mychaint@gmail.com>
2016-12-30 09:44:18 +0800

    update

--- OthelloAI.py
+++ OthelloAI.py
@@ -1,5 +1,7 @@
 import Othello
 import pandas as pd
+from os import listdir
+from os.path import isfile, join
 
 class OthelloAI:
     def __init__(self, gamehost):
@@ -7,8 +9,19 @@ class OthelloAI:
         else: self.gamehost = Othello(True)
     
     def load_play_records(self):
-        
-    
+        record_names = [f for f in listdir('game_records') if isfile(join('game_records', f))]
+        for filename in record_names:
+            record = open(filename)
+            string = record.readline()
+            n = 1
+            dataset = {}
+            data = []
+            while string:
+                string = record.readline()
+                data.append([x for x in string])
+                if i % 8 == 0:
+                    dataset[]
+                            
     def find_a_move(self, player, board):
         pass
     
